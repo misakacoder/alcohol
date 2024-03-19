bc07593b8ae93ca40147794d81cd3daed710da90
misakacoder <210086826@qq.com>
2024-03-19 12:17:04 +0800

    init

--- spring-boot-starter/auth-spring-boot-starter/src/main/java/com/misaka/filter/BasicAuthFilter.java
+++ spring-boot-starter/auth-spring-boot-starter/src/main/java/com/misaka/filter/BasicAuthFilter.java
@@ -3,16 +3,13 @@ package com.misaka.filter;
 import com.misaka.annotation.BasicAuth;
 import com.misaka.annotation.DisableBasicAuth;
 import com.misaka.properties.BasicAuthProperties;
-import org.apache.catalina.connector.RequestFacade;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.util.Base64Utils;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
-import org.springframework.web.servlet.mvc.condition.RequestMethodsRequestCondition;
+import org.springframework.web.servlet.HandlerExecutionChain;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
@@ -21,15 +18,19 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 @Order(Integer.MIN_VALUE)
 public class BasicAuthFilter implements InitializingBean, Filter {
 
     private static final String AUTH_FAILED_MESSAGE = "authentication failure";
-    private static final String BASIC_AUTH_URL_FORMAT = "%s %s";
-    private static final List<String> BASIC_AUTH_URL_LIST = new ArrayList<>();
+
+    private final Set<Method> authMethods = new HashSet<>();
 
     @Autowired
     private BasicAuthProperties basicAuthProperties;
@@ -41,31 +42,36 @@ public class BasicAuthFilter implements InitializingBean, Filter {
     public void afterPropertiesSet() throws Exception {
         Map<RequestMappingInfo, HandlerMethod> handlerMethodMap = requestMappingHandlerMapping.getHandlerMethods();
         handlerMethodMap.forEach((k, v) -> {
-            Class<?> controllerClass = v.getMethod().getDeclaringClass();
+            Method method = v.getMethod();
+            Class<?> controllerClass = method.getDeclaringClass();
             boolean basicAuth = v.hasMethodAnnotation(BasicAuth.class) || controllerClass.getAnnotation(BasicAuth.class) != null;
             boolean disableBasicAuth = v.hasMethodAnnotation(DisableBasicAuth.class);
             if (basicAuth && !disableBasicAuth) {
-                RequestMethodsRequestCondition methodsCondition = k.getMethodsCondition();
-                PatternsRequestCondition patternsCondition = k.getPatternsCondition();
-                if (patternsCondition != null) {
-                    RequestMethod method = getFirst(methodsCondition.getMethods());
-                    String path = getFirst(patternsCondition.getPatterns());
-                    BASIC_AUTH_URL_LIST.add(String.format(BASIC_AUTH_URL_FORMAT, method, path));
-                }
+                authMethods.add(method);
             }
         });
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        RequestFacade requestFacade = (RequestFacade) servletRequest;
-        String basicAuthUrl = String.format(BASIC_AUTH_URL_FORMAT, requestFacade.getMethod(), requestFacade.getServletPath());
-        if (BASIC_AUTH_URL_LIST.contains(basicAuthUrl)) {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        Method method = null;
+        try {
+            HandlerExecutionChain handlerExecutionChain = requestMappingHandlerMapping.getHandler(httpServletRequest);
+            if (handlerExecutionChain != null) {
+                Object handler = handlerExecutionChain.getHandler();
+                if (handler instanceof HandlerMethod) {
+                    method = ((HandlerMethod) handler).getMethod();
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        if (authMethods.contains(method)) {
             String username = basicAuthProperties.getUsername();
             String password = basicAuthProperties.getPassword();
             byte[] bytes = String.format("%s:%s", username, password).getBytes(StandardCharsets.UTF_8);
             String authorization = "Basic " + Base64Utils.encodeToString(bytes);
-            HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
             String header = httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION);
             if (!authorization.equals(header)) {
                 HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
