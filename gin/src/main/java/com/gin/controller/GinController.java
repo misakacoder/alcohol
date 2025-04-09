8f9dd7c6eb056c0c8b5c0f5a7ed6c521792d5623
misakacoder <210086826@qq.com>
2025-04-09 17:09:15 +0800

    init

--- gin/src/main/java/com/gin/controller/GinController.java
+++ gin/src/main/java/com/gin/controller/GinController.java
@@ -10,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,9 +27,6 @@ public class GinController {
 
     @Autowired
     private Hero hero;
-
-    @Autowired
-    private Environment environment;
 
     @Autowired
     private ServerProperties serverProperties;
